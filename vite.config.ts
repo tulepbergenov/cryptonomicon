5ae10f04462da6591c9e1f502f1c652c9501fa9b
Daulet Tulepbergenov <daulet6368@gmail.com>
2023-12-23 02:45:10 +0600

    chore: add linters, alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,15 @@
-import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import { fileURLToPath } from "node:url";
+import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: [
+      {
+        find: "@",
+        replacement: fileURLToPath(new URL("./src", import.meta.url)),
+      },
+    ],
+  },
 });
