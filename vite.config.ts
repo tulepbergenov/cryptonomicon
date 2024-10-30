d510aaa00a145ad6970f743fcc775d794a1efc1b
Daulet Tulepbergenov <daulet6368@gmail.com>
2024-10-30 23:53:14 +0500

    dev: up vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import path from "path";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,5 +9,24 @@ export default defineConfig({
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  build: {
+    outDir: "build",
+  },
+  server: {
+    open: true,
+    strictPort: true,
+    port: 4010,
+    host: true,
+    hmr: {
+      port: 4010,
+      clientPort: 4010,
+    },
+  },
+  preview: {
+    open: true,
+    strictPort: true,
+    port: 4014,
+    host: true,
   },
 });
